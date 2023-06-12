f7fbd82190fdf9296c4e83ade6f361c57a90ae51
fatrbaby <fatrbaby@qq.com>
2023-06-12 11:59:19 +0800

    (upgrade) refctor

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,9 @@ use std::env::current_dir;
 use project::Project;
 
 fn main() {
-    let p = Project::new(get_cwd());
-
-    p.show_work_dir();
-}
-
-fn get_cwd() -> String {
     let cwd = current_dir().expect("can not get current path");
-    cwd.to_string_lossy().to_string()
+    let p = Project::new(cwd.as_path());
+
+    p.show_setting();
 }
+
--- src/project/mod.rs
+++ src/project/mod.rs
@@ -2,69 +2,76 @@ use std::fs;
 use std::fs::File;
 use std::io::Write;
 use directories::BaseDirs;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 use sha2::{Sha256, Digest};
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug)]
 pub struct Project {
-    conf: Config,
+    setting: Setting,
 }
 
 impl Project {
-    pub fn new(workdir: String) -> Project {
-        let mut home = "~".to_owned();
-
-        if let Some(base_dir) = BaseDirs::new() {
-            home = (&base_dir.home_dir().to_string_lossy()).parse().unwrap();
-        }
-
-        Project {
-            conf: Config::of(home, workdir),
+    pub fn new(workdir: &Path) -> Project {
+        match BaseDirs::new() {
+            None => {
+                Project {
+                    setting: Setting::of(Path::new("~"), workdir),
+                }
+            }
+            Some(base_dir) => {
+                Project {
+                    setting: Setting::of(base_dir.home_dir(), workdir),
+                }
+            }
         }
     }
 }
 
 impl Project {
-    pub fn show_work_dir(&self) {
-        println!("{:?}", self.conf);
+    pub fn show_setting(&self) {
+        println!("{:?}", self.setting);
     }
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-pub struct Config {
+pub struct Setting {
     php_version: String,
     php_binary: String,
     port: u16,
     index: String,
 }
 
-impl Config {
-    pub fn of(file_path: String, workdir: String) -> Config {
+impl Setting {
+    pub fn of(path: &Path, workdir: &Path) -> Setting {
         let mut hasher = Sha256::new();
-        hasher.update(workdir);
+        hasher.update(workdir.to_string_lossy().as_bytes());
         let hash = format!("{:X}", hasher.finalize());
-        let setting_path = format!("{}/{}/{}/setting.json", file_path, ".cynic", hash);
-        let f = Path::new(&setting_path);
 
-        if f.exists() {
-            return load_config_from(f.to_string_lossy().to_string());
+        let mut setting_path = PathBuf::new();
+        setting_path.push(path);
+        setting_path.push(".cynic");
+        setting_path.push(hash);
+        setting_path.push("setting.json");
+
+        if setting_path.exists() {
+            return load_config_from(setting_path.as_path());
         }
 
-        create_new_config(f.to_string_lossy().to_string())
+        create_new_config(setting_path.as_path())
     }
 }
 
-fn load_config_from(path: String) -> Config {
+fn load_config_from(path: &Path) -> Setting {
     let setting = fs::read_to_string(path).unwrap();
 
-    let conf: Config = serde_json::from_str(&setting).unwrap();
+    let conf: Setting = serde_json::from_str(&setting).unwrap();
 
     conf
 }
 
-fn create_new_config(path: String) -> Config {
-    let conf = Config {
+fn create_new_config(path: &Path) -> Setting {
+    let conf = Setting {
         php_version: "8.0".to_string(),
         php_binary: "/usr/local/bin/php".to_string(),
         port: 7788,
@@ -72,8 +79,6 @@ fn create_new_config(path: String) -> Config {
     };
 
     let json = serde_json::to_string(&conf).expect("build config json fail.");
-
-    let path = Path::new(&path);
 
     if let Some(dir) = path.parent() {
         if !dir.exists() {
@@ -94,14 +99,4 @@ fn create_new_config(path: String) -> Config {
 }
 
 #[cfg(test)]
-mod tests {
-    use sha2::{Digest, Sha256};
-    use hex_literal::hex;
-
-    #[test]
-    fn test_project_hash() {
-        let result = Sha256::digest(b"/Users/fatrbaby/workspaces/rust/cynic");
-
-        assert!(result[..], hex!("4eaecc89ede85b11f75192aad974c9be097e30937d8a6ff2a31593db190a54bd"));
-    }
-}
+mod tests {}
